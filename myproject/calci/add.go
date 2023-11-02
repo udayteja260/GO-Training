309da361a68e534cfff3fb6f54762f5c3b0bc0d9
udayteja260 <udayteja260@gmail.com>
2023-11-02 16:45:46 -0500

    First Commit

---
+++ myproject/calci/add.go
@@ -0,0 +1,5 @@
+package calci
+
+func Add(a, b int) int {
+	return a + b
+}
---
+++ myproject/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"myproject/calci"
+)
+
+func main() {
+	a, b := 5, 6
+	fmt.Println("hello world")
+	fmt.Println(calci.Add(a, b))
+
+}
